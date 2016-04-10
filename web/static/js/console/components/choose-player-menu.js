f1c96a2375ff5606ce08263574a496505e1bc8c8
David Corwin <dave@launchpadlab.com>
2016-04-10 12:46:45 -0500

    added choose player menu

---
+++ web/static/js/console/components/choose-player-menu.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes as PT } from 'react'
+import Page from 'page'
+import PlayerMenu from './player-menu'
+
+class ChoosePlayerMenu extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isReady: false,
+      players: [],
+      player1: null,
+      player2: null
+    }
+  }
+
+  componentDidMount() {
+    // return {
+    //   players: Players.find({}, { sort: { name: 1 } }).fetch()
+    // };
+    // // fetchplayers.then(players => this.setState(isReady: true, players: players))
+  }
+
+  componentWillUpdate(_, nextState) {
+    const { player1, player2 } = this.state
+
+    if (player1 && player2) {
+      //create a new game then
+      //Page('game/:id')
+    }
+  }
+
+  // selectPlayer(player1OrPlayer2, player) {
+  //   this[player1OrPlayer2] = player;
+  //
+  //   if (this.player1 && this.player2) {
+  //     Meteor.call("newGame", this.player1, this.player2, (_,gameId) => {
+  //       Page(`/game/${gameId}`);
+  //     });
+  //   }
+  // },
+
+  selectPlayer(player, item) {
+    this.setState({ [player]: item.player })
+  }
+
+  unSelectPlayer(player) {
+    this.setState({ [player]: null })
+  }
+
+  render() {
+
+    const { players } = this.state
+
+    const menuItems = players.map(player => {
+      return { title: player.name, player: player }
+    })
+
+    return (
+      <div className="game-container">
+        <PlayerMenu
+          menuItems={menuItems}
+          listens={[KeyCode.Left]}
+          onSelect={this.selectPlayer.bind(this, 'player1')}
+          onUnSelect={this.unSelectPlayer.bind(this, 'player1')}
+          side="left"
+        />
+        <PlayerMenu
+          menuItems={menuItems}
+          listens={[KeyCode.Right]}
+          handleSelect={this.selectPlayer.bind(this, 'player2')}
+          handleUnSelect={this.unSelectPlayer.bind(this, 'player2')}
+          side="right"
+        />
+      </div>
+    )
+  }
+}
+
+export default ChoosePlayerMenu
