a095d5cc136dd4c8e82071be70fa1cec3f15b4de
David Corwin <dave@launchpadlab.com>
2016-04-21 09:30:26 -0500

    update server api url

--- web/static/js/console/lib/api.js
+++ web/static/js/console/lib/api.js
@@ -1,7 +1,11 @@
 import Axios from 'axios'
 
+const baseURL = window.location.hostname === 'localhost' ?
+  'http://localhost:4000/api/' :
+  'https://phx-pong.herokuapp.com/api/'
+
 const Api = Axios.create({
-  baseURL: 'http://localhost:4000/api/',
+  baseURL: baseURL,
   timeout: 1000
 })
 
